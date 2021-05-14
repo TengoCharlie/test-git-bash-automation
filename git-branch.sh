d812a48ff31ebb093412350a021ad3447502bf5a
Harsh Tenguriya <thegreatharsh1@gmail.com>
2021-05-14 22:30:44 +0530

    My First Commit

---
+++ git-branch.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+addFileDefault="."
+addFileDefaultTemp=""
+commitMessage="My First Commit"
+commitMessageTemp=""
+branchName="main"
+branchNameTemp=""
+upStream=""
+upStreamTemp="N"
+
+echo -e "Please enter file name to make git: ($addFileDefault)"
+read addFileDefaultTemp
+echo -e "Please Enter Commit message: ($commitMessage)"
+read commitMessageTemp
+echo -e "Please enter Branch name: ($branchName)"
+read branchNameTemp
+echo -e "Please select --set-upstream branch: (N)(Y/N)"
+read upStreamTemp
+
+if [ "$addFileDefaultTemp" != "" ]; then
+    echo -e "File added is: $addFileDefaultTemp"
+    addFileDefault=$addFileDefaultTemp
+fi
+
+if [ "$commitMessageTemp" != "" ]; then
+    echo -e "Your Commit message is: $addFileDefaultTemp"
+    commitMessage=$commitMessageTemp
+fi
+
+if [ "$branchNameTemp" != "" ]; then
+    echo -e "Your Branch name is: $branchNameTemp"
+    branchName=$branchNameTemp
+fi
+if [ "$upStreamTemp" == "Y" ]; then
+    echo -e "Your --set-upstream branch is: --set-upstream"
+    upStream="--set-upstream"
+fi
+
+
+git branch $branchName
+git checkout $branchName
+git add $addFileDefault
+git commit -m "$commitMessage"
+git push $upStream origin $branchName
